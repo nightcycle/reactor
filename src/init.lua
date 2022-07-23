97058a5304ae42d87ed386cb82b5c7f3a5daeb3b
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2022-07-22 21:02:46 -0400

    simplified stuff a ton

--- src/init.lua
+++ src/init.lua
@@ -1,127 +1,144 @@
-local RunService = game:GetService("RunService")
+--!strict
+local RunService: RunService = game:GetService("RunService")
+local Maid = require(script.Parent.Maid)
 
-local Game = require(script:WaitForChild("Game"))
-local Util = require(script:WaitForChild("Util"))
-local Service = require(script:WaitForChild("Service"))
+if RunService:IsServer() then
+	local RemFuncFolder: Instance = if script:FindFirstChild("RemoteFunctions") then script:FindFirstChild("RemoteFunctions") else Instance.new("Folder")
+	RemFuncFolder.Name = "RemoteFunctions"
+	RemFuncFolder.Parent = script
+
+	local RemEvFolder: Instance = if script:FindFirstChild("RemoteEvents") then script:FindFirstChild("RemoteEvents") else Instance.new("Folder", script)
+	RemEvFolder.Name = "RemoteEvents"
+	RemEvFolder.Parent = script
+	
+	local BindFuncFolder: Instance = if script:FindFirstChild("BindableFunctions") then script:FindFirstChild("BindableFunctions") else Instance.new("Folder", script)
+	BindFuncFolder.Name = "BindableFunctions"
+	BindFuncFolder.Parent = script
+
+	local BindEvFolder: Instance = if script:FindFirstChild("BindableEvents") then script:FindFirstChild("BindableEvents") else Instance.new("Folder", script)
+	BindEvFolder.Name = "BindableEvents"
+	BindEvFolder.Parent = script
+end
 
 local Reactor = {}
-setmetatable(Util, Game)
-setmetatable(Service, Util)
-setmetatable(Reactor, Service)
+Reactor.__index = Reactor
 
 function Reactor:Destroy(): nil
-	Service.Destroy(self)
-
-	setmetatable(self, nil)
+	self._Maid:Destroy()
 	for k, v in pairs(self) do
 		self[k] = nil
 	end
+	setmetatable(self, nil)
+	return nil
 end
 
-Reactor.__index = function(s, k)
-	local v = rawget(s, k)
-	local cV = rawget(s, "_Config")[k]
-	if v then
-		return v
-	elseif cV then
-		return cV
-	elseif Reactor[k] then
-		return Reactor[k]
-	elseif Service[k] then
-		return Service[k]
-	elseif Util[k] then
-		return Util[k]
-	elseif Game[k] then
-		return Game[k]
-	else
-		return game[k]
+function Reactor.typeof(obj): string
+	local nativeType = typeof(obj)
+	if nativeType == "table" then
+		return obj.Type or obj._Type or obj.type or obj._type or obj.__type
+	elseif typeof(obj) == "Instance" then
+		return obj.ClassName
 	end
+	return typeof(obj)
 end
 
-Reactor.__newindex = function(s, k, v)
-	error("Reactor is locked, because uh, radiation or something")
+function Reactor:IsServer(): boolean
+	return RunService:IsServer()
 end
 
-
-function prepConfig(config)
-	config = config or {}
-
-	config.Version = config.Version or "Unspecified"
-	config.Logging = config.Logging or {
-		Levels = {
-			Debug = RunService:IsStudio(),
-			Info = RunService:IsStudio(),
-			Warning = true,
-			Error = true,
-			Fatal = true,
-		},
-		Filters = {}
-	}
-
-	return config
+function Reactor:IsClient(): boolean
+	return RunService:IsClient()
 end
 
-function extractAnalyticsKeys(config): (string | nil, string | nil)
-	local titleId: string | nil
-	local secretKey: string | nil
+function Reactor:IsStudio(): boolean
+	return RunService:IsStudio()
+end
 
+function Reactor:IsRunning(): boolean
+	return RunService:IsRunning()
+end
+
+function Reactor:GiveTask(disposable: ({Destroy: (self: any) -> nil}) | Instance | RBXScriptConnection): nil
+	self._Maid:GiveTask(disposable)
+end
+
+function Reactor:GetRemoteEvent(eventName): RemoteEvent
+	local remoteEvents = script:WaitForChild("RemoteEvents")
 	if RunService:IsServer() then
-		titleId = config.TitleId
-		secretKey = config.DevSecretKey
-		config.TitleId = nil
-		config.DevSecretKey = nil
-	end
+		if remoteEvents:FindFirstChild(eventName) then
+			local remEvent: Instance? = remoteEvents:WaitForChild(eventName)
+			assert(remEvent ~= nil and remEvent:IsA("RemoteEvent"))
+			return remEvent
+		else
+			local remoteEvent: RemoteEvent = Instance.new("RemoteEvent")
+			remoteEvent.Name = eventName
+			remoteEvent.Parent = remoteEvents
 
-	return titleId, secretKey
+			return remoteEvent
+		end
+	else
+		local remEvent: Instance? = remoteEvents:WaitForChild(eventName)
+		assert(remEvent ~= nil and remEvent:IsA("RemoteEvent"))
+		return remEvent
+	end
+end
+
+function Reactor:GetRemoteFunction(functionName): RemoteFunction
+	local remoteFunctions = script:WaitForChild("RemoteFunctions")
+	if RunService:IsServer() then
+		if remoteFunctions:FindFirstChild(functionName) then
+			return remoteFunctions:FindFirstChild(functionName)
+		else
+			local remoteFunction = Instance.new("RemoteFunction")
+			remoteFunction.Name = functionName
+			remoteFunction.Parent = remoteFunctions
+			return remoteFunction
+		end
+	else
+		return remoteFunctions:WaitForChild(functionName)
+	end
 end
 
 
-return function(config)
-	config = prepConfig(config)
-	local titleId, secretKey = extractAnalyticsKeys(config)
-	local self = {
-		_Config = config,
-		_Importer = nil,
-		_Services = {},
-		_ServiceCaches = {},
-		Instance = {}
-	}
-	setmetatable(self, Reactor)
-	self:SetPackages()
-	if RunService:IsServer() and titleId and secretKey then
-		local analytics = self:GetService("Analytics")
-		analytics.init(titleId, secretKey)
-		self:CacheService("Analytics", analytics)
-	end
-
-	local Enum = self:GetService("Enum")
-	Enum.Log = {"Debug", "Info", "Warn", "Error", "Fatal"}
-	self:CacheService("Enum", Enum)
-
-	local Importer = self:GetService("import")
-	Importer.setConfig({
-		useWaitForChild = true,
-		scriptAlias = "*",
-	})
-	Importer.setAliases({
-		server = if RunService:IsServer() then game.ServerScriptService:WaitForChild("Server") else nil,
-		client = if RunService:IsClient() then
-			if RunService:IsRunning() then
-				game.Players.LocalPlayer:WaitForChild("PlayerScripts"):WaitForChild("Client")
-			else
-				game:WaitForChild("StarterPlayer"):WaitForChild("StarterPlayerScripts"):WaitForChild("Client")
-		else nil,
-		shared = game.ReplicatedStorage:WaitForChild("Shared"),
-		library = game.ReplicatedStorage:WaitForChild("Library"),
-		first = if RunService:IsClient() then game.ReplicatedFirst else nil,
-		workspace = workspace,
-	})
-	rawset(self, "_Importer", Importer)
-	if self.Instance then
-		self.Instance.new = function(...)
-			return self:BuildService(...)
+function Reactor:GetBindableEvent(eventName): BindableEvent
+	local bindableEvents = script:WaitForChild("BindableEvents")
+	if RunService:IsServer() then
+		if bindableEvents:FindFirstChild(eventName) then
+			return bindableEvents:FindFirstChild(eventName)
+		else
+			local bindableEvent = Instance.new("BindableEvent")
+			bindableEvent.Name = eventName
+			bindableEvent.Parent = bindableEvents
+			return bindableEvent
 		end
+	else
+		return bindableEvents:WaitForChild(eventName)
 	end
+end
 
+function Reactor:GetBindableFunction(functionName): BindableFunction
+	local bindableFunctions = script:WaitForChild("BindableFunctions")
+	if RunService:IsServer() then
+		if bindableFunctions:FindFirstChild(functionName) then
+			return bindableFunctions:FindFirstChild(functionName)
+		else
+			local bindableFunction = Instance.new("BindableFunction")
+			bindableFunction.Name = functionName
+			bindableFunction.Parent = bindableFunctions
+			return bindableFunction
+		end
+	else
+		return bindableFunctions:WaitForChild(functionName)
+	end
+end
+
+function Reactor.new()
+	local self = setmetatable({
+		_Maid = Maid.new()
+	}, Reactor)
 	return self
 end
+
+export type Reactor = typeof(Reactor.new())
+
+return Reactor
